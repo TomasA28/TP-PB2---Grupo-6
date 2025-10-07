9805d104458738998e37533b82e2386cc04addf3
aixa <aixamuriel736@gmail.com>
2025-10-07 16:27:08 -0300

    verificar el precio de catering segun la cantidad de empleados

--- src/clasesProductivasDecoracionDeEventos/ServicioAdicional.java
+++ src/clasesProductivasDecoracionDeEventos/ServicioAdicional.java
@@ -1,8 +1,12 @@
 package clasesProductivasDecoracionDeEventos;
 
-public class ServicioAdicional {
-	//ATRIBUTOS
-	//CONSTRUCTOR
-	//METODOS
-	
+public abstract class ServicioAdicional {
+
+	// ATRIBUTOS
+
+	// METODOS
+	public Double calcularPrecio(Integer cantDeInvitados) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
