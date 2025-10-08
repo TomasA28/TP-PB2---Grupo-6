78d24e7287a182cc869b93eaf1e80d217f0077ed
ariiflores <afloresromero643@alumno.unlam.edu.ar>
2025-10-07 21:26:19 -0300

    se agrego clase animacion adultos

--- src/clasesProductivasDecoracionDeEventos/AnimacionAdultos.java
+++ src/clasesProductivasDecoracionDeEventos/AnimacionAdultos.java
@@ -1,6 +1,11 @@
 package clasesProductivasDecoracionDeEventos;
 
+import java.util.Objects;
+
 public class AnimacionAdultos implements ServicioAdicional {
+
+	private Double PrecioBase = 20000.0;
+	private String tematicasDeAnimacion;
 
 	public AnimacionAdultos() {
 		// TODO Auto-generated constructor stub
@@ -8,8 +13,44 @@ public class AnimacionAdultos implements ServicioAdicional {
 
 	@Override
 	public Double calcularPrecio(Integer cantDeInvitados) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Double precio;
+		if (cantDeInvitados < 20) {
+			precio = this.PrecioBase;
+		} else {
+			precio = PrecioBase + 20000.0;
+		}
+		return precio;
+
 	}
+
+	public Double getPrecioBase() {
+		return PrecioBase;
+	}
+
+	public String getTematicasDeAnimacion() {
+		return tematicasDeAnimacion;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(PrecioBase, tematicasDeAnimacion);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnimacionAdultos other = (AnimacionAdultos) obj;
+		return Objects.equals(PrecioBase, other.PrecioBase)
+				&& Objects.equals(tematicasDeAnimacion, other.tematicasDeAnimacion);
+	}
+	
+	
+
 
 }
